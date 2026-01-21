3fa297290ae8e9d52c1d29de16160f743a843e78
Oshaq Naimat <oshaqnaimat15@gmail.com>
2026-01-21 23:04:32 +0500

    backend

---
+++ app/Backend/model/messageModal.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose"
+
+const messageSchema = mongoose.Schema({
+    chats:{
+        type:Array,
+        default:[]
+    },
+    sender_id:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:'user'
+    },
+    receiver_id:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:'user'
+    }
+},{
+    timestamps:true
+})
+
+
+export const Messages = mongoose.model('messages',messageSchema)
